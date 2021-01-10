72307adfb8e9b0c428662ab5b73f4e54593ebbe0
Liga <ligabaikova@gmail.com>
2021-01-10 01:38:01 +0000

    Update allowed hosts in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['django-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
